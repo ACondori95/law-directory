ff9feb32d1036ce4da0533e17d9240f6566c5650
Alvaro Condori <con21033@byui.edu>
2024-05-20 19:00:54 -0300

    add PUT, POST, and DELETE

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,3 +1,4 @@
+const {response} = require("express");
 const mongodb = require("../data/database");
 const ObjectId = require("mongodb").ObjectId;
 
@@ -22,4 +23,72 @@ const getSingleUser = async (req, res) => {
   });
 };
 
-module.exports = {getAllUsers, getSingleUser};
+const createNewUser = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    password: req.body.password,
+    email: req.body.email,
+    clients: req.body.clients,
+  };
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("users")
+    .insertOne(user);
+  if (response.acknowledged) {
+    res.status(204).json({message: "New user was created successfully!"});
+  } else {
+    res
+      .status(500)
+      .json(response.error || "Some error occurred while creating the user");
+  }
+};
+
+const updateUser = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    password: req.body.password,
+    email: req.body.email,
+    clients: req.body.clients,
+  };
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("users")
+    .replaceOne({_id: userId}, user);
+  if (response.modifiedCount > 0) {
+    res.status(204).json({message: "User was updated successfully."});
+  } else {
+    res
+      .status(500)
+      .json(response.error || "Some error occurred while updating the user.");
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const response = await mongodb
+    .getDatabase()
+    .db()
+    .collection("users")
+    .deleteOne({_id: userId});
+  if (response.deletedCount > 0) {
+    res.status(204).json({message: "User deleted successfully."});
+  } else {
+    res
+      .status(500)
+      .json(response.error || "Some error occurred while deleting the user.");
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  getSingleUser,
+  createNewUser,
+  updateUser,
+  deleteUser,
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,5 +5,8 @@ const userControllers = require("../controllers/user.controllers");
 
 router.get("/", userControllers.getAllUsers);
 router.get("/:userId", userControllers.getSingleUser);
+router.post("/", userControllers.createNewUser);
+router.put("/:userId", userControllers.updateUser);
+router.delete("/:userId", userControllers.deleteUser);
 
 module.exports = router;
